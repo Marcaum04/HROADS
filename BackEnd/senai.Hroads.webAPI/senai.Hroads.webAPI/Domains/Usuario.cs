c08fcb7b72dcd19fac72be6125f225b6d3f2a0f2
Marcos <mvinicius.oliveira04@gmail.com>
2021-09-27 11:16:18 -0300

    Jwt finalizado

--- BackEnd/senai.Hroads.webAPI/senai.Hroads.webAPI/Domains/Usuario.cs
+++ BackEnd/senai.Hroads.webAPI/senai.Hroads.webAPI/Domains/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -15,7 +16,12 @@ namespace senai.Hroads.webAPI.Domains
         public int IdUsuario { get; set; }
         public byte? IdTipoUsuario { get; set; }
         public string NomeUsuario { get; set; }
+
+        [Required(ErrorMessage = "O campo e-mail é obrigatório!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório!")]
+        [StringLength(9, MinimumLength = 3, ErrorMessage = "A senha deve ter de 3 a 10 caracteres")]
         public string Senha { get; set; }
 
         public virtual Tipousuario IdTipoUsuarioNavigation { get; set; }
