60b4f6e4ecb3931eef32d7c91d00b277620598e9
Marcos <mvinicius.oliveira04@gmail.com>
2021-09-27 08:35:50 -0300

    Swagger feito

---
+++ BackEnd/senai.Hroads.webAPI/senai.Hroads.webAPI/Context/HroadsContext.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using senai.Hroads.webAPI.Domains;
+
+#nullable disable
+
+namespace senai.Hroads.webAPI.Context
+{
+    public partial class HroadsContext : DbContext
+    {
+        public HroadsContext()
+        {
+        }
+
+        public HroadsContext(DbContextOptions<HroadsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Classe> Classes { get; set; }
+        public virtual DbSet<Classehabilidade> Classehabilidades { get; set; }
+        public virtual DbSet<Habilidade> Habilidades { get; set; }
+        public virtual DbSet<Personagem> Personagems { get; set; }
+        public virtual DbSet<Tipohabilidade> Tipohabilidades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=NOTE0113C5\\SQLEXPRESS; Initial Catalog=SENAI_HROADS_MANHA; user id=sa; pwd=Senai@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Classe>(entity =>
+            {
+                entity.HasKey(e => e.IdClasse)
+                    .HasName("PK__CLASSE__60FFF8018029AAF0");
+
+                entity.ToTable("CLASSE");
+
+                entity.HasIndex(e => e.NomeC, "UQ__CLASSE__F76AAB26FAFEC05E")
+                    .IsUnique();
+
+                entity.Property(e => e.IdClasse)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idClasse");
+
+                entity.Property(e => e.NomeC)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("nomeC");
+            });
+
+            modelBuilder.Entity<Classehabilidade>(entity =>
+            {
+                entity.HasKey(e => e.IdClasseHabilidade)
+                    .HasName("PK__CLASSEHA__5FC969724433421B");
+
+                entity.ToTable("CLASSEHABILIDADE");
+
+                entity.Property(e => e.IdClasseHabilidade)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idClasseHabilidade");
+
+                entity.Property(e => e.IdClasse).HasColumnName("idClasse");
+
+                entity.Property(e => e.IdHabilidade).HasColumnName("idHabilidade");
+
+                entity.HasOne(d => d.IdClasseNavigation)
+                    .WithMany(p => p.Classehabilidades)
+                    .HasForeignKey(d => d.IdClasse)
+                    .HasConstraintName("FK__CLASSEHAB__idCla__32E0915F");
+
+                entity.HasOne(d => d.IdHabilidadeNavigation)
+                    .WithMany(p => p.Classehabilidades)
+                    .HasForeignKey(d => d.IdHabilidade)
+                    .HasConstraintName("FK__CLASSEHAB__idHab__31EC6D26");
+            });
+
+            modelBuilder.Entity<Habilidade>(entity =>
+            {
+                entity.HasKey(e => e.IdHabilidade)
+                    .HasName("PK__HABILIDA__655F75285660FAB3");
+
+                entity.ToTable("HABILIDADE");
+
+                entity.HasIndex(e => e.NomeH, "UQ__HABILIDA__F76AAB233263D23C")
+                    .IsUnique();
+
+                entity.Property(e => e.IdHabilidade)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idHabilidade");
+
+                entity.Property(e => e.IdTipoHabilidade).HasColumnName("idTipoHabilidade");
+
+                entity.Property(e => e.NomeH)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("nomeH");
+
+                entity.HasOne(d => d.IdTipoHabilidadeNavigation)
+                    .WithMany(p => p.Habilidades)
+                    .HasForeignKey(d => d.IdTipoHabilidade)
+                    .HasConstraintName("FK__HABILIDAD__idTip__2F10007B");
+            });
+
+            modelBuilder.Entity<Personagem>(entity =>
+            {
+                entity.HasKey(e => e.IdPersonagem)
+                    .HasName("PK__PERSONAG__4C5EDFB30B41F183");
+
+                entity.ToTable("PERSONAGEM");
+
+                entity.HasIndex(e => e.NomeP, "UQ__PERSONAG__F76AACDB3BCBBADF")
+                    .IsUnique();
+
+                entity.Property(e => e.IdPersonagem).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.DataAtuailizacao)
+                    .HasColumnType("date")
+                    .HasColumnName("dataAtuailizacao");
+
+                entity.Property(e => e.DataCriacao)
+                    .HasColumnType("date")
+                    .HasColumnName("dataCriacao");
+
+                entity.Property(e => e.IdClasse).HasColumnName("idClasse");
+
+                entity.Property(e => e.ManaMaxima).HasColumnName("manaMaxima");
+
+                entity.Property(e => e.NomeP)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("nomeP");
+
+                entity.Property(e => e.VidaMaxima).HasColumnName("vidaMaxima");
+
+                entity.HasOne(d => d.IdClasseNavigation)
+                    .WithMany(p => p.Personagems)
+                    .HasForeignKey(d => d.IdClasse)
+                    .HasConstraintName("FK__PERSONAGE__idCla__36B12243");
+            });
+
+            modelBuilder.Entity<Tipohabilidade>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoHabilidade)
+                    .HasName("PK__TIPOHABI__B197B832648CD0F2");
+
+                entity.ToTable("TIPOHABILIDADE");
+
+                entity.HasIndex(e => e.NomeTh, "UQ__TIPOHABI__77FC528A1DE7B64C")
+                    .IsUnique();
+
+                entity.Property(e => e.IdTipoHabilidade)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idTipoHabilidade");
+
+                entity.Property(e => e.NomeTh)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("nomeTH");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- BackEnd/senai.Hroads.webAPI/senai.Hroads.webAPI/Startup.cs
+++ BackEnd/senai.Hroads.webAPI/senai.Hroads.webAPI/Startup.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai.Hroads.webAPI
@@ -16,6 +20,28 @@ namespace senai.Hroads.webAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Hroads.webAPI",
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +52,22 @@ namespace senai.Hroads.webAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hroads.webAPI V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
